994e50489dc2f01713fc53d39f629573ebf3b9bf
Eduardo Rambauske <dudurpp1@gmail.com>
2025-01-28 15:42:28 -0300

    change endpoints to act on database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_return_ok_and_helloworld(client):
     response = client.get('/')
@@ -26,29 +28,53 @@ def test_create_user(client):
     }
 
 
+def test_create_invalid_user_with_existing_username(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': user.username,
+            'email': 'alice@gmail.com',
+            'password': 'wonderland123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Username Already Exists.'}
+
+
+def test_create_invalid_user_with_existing_email(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': user.email,
+            'password': 'wonderland123',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'detail': 'Email Already Exists.'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'alice',
-                'email': 'alice@gmail.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user(client, user):
     response = client.get('/users/1')
+    user_schema = UserPublic.model_validate(user).model_dump()
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'alice',
-        'email': 'alice@gmail.com',
-    }
+    assert response.json() == user_schema
 
 
 def test_read_invalid_user_then_return_404(client):
@@ -57,7 +83,7 @@ def test_read_invalid_user_then_return_404(client):
     assert response.json() == {'detail': 'User Not Found.'}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -74,6 +100,33 @@ def test_update_user(client):
     }
 
 
+def test_update_integrity_error(client, user):
+    # Criando um registro para "fausto"
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    # Alterando o user.username das fixture para fausto
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email Already Exists.'
+    }
+
+
 def test_update_invalid_user_then_return_404(client):
     response = client.put(
         '/users/0',
@@ -87,7 +140,7 @@ def test_update_invalid_user_then_return_404(client):
     assert response.json() == {'detail': 'User Not Found.'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
