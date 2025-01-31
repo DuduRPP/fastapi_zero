d4383966906639ad7d28c4f401e2db63aa380222
Eduardo Rambauske <dudurpp1@gmail.com>
2025-01-30 23:57:47 -0300

    refactor: added routers for api

--- tests/test_app.py
+++ tests/test_app.py
@@ -164,7 +164,7 @@ def test_delete_unauthorized_user_then_return_404(client, user, token):
 
 def test_get_token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     token = response.json()
